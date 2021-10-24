ead9ede2d340cdc2857d5409660f4bda1353e19d
phedayat <phedayat@ucsd.edu>
2021-10-23 18:58:09 -0700

    Added comments, fixed timeout for smiling face, added press to talk functionality.

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,58 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+// Get the voice object from the list of voices
+function getVoiceObject(voices, name){
+  for(let i = 0; i < voices.length; i++){
+    if(voices[i].name === name){
+      return voices[i];
+    }
+  }
+}
+
+// Timeout if the SpeechSynthesis object is "speaking"
+function checkIfSpeaking(ss, faceImg){
+  setTimeout(() => {
+    if(ss.speaking){
+      faceImg.src = "assets/images/smiling-open.png";
+      checkIfSpeaking(ss, faceImg);
+    }else{
+      faceImg.src = "assets/images/smiling.png";
+    }
+  }, 150);
+}
+
 function init() {
-  // TODO
+  // Get relevant HTML elements
+  let pressToTalkBtn = document.getElementsByTagName("button")[0];
+  let voiceSelect = document.getElementById("voice-select");
+  let faceImg = document.getElementsByTagName("img")[0];
+  
+  // Setup utterance
+  var utterance = new SpeechSynthesisUtterance();
+  var voices = [];
+  var ss = window.speechSynthesis;
+
+  // Get and set the possible voices
+  ss.addEventListener('voiceschanged', () => {
+    voices = ss.getVoices();
+    for(let i = 0; i < voices.length; i++){
+      let voice = document.createElement("option");
+      voice.textContent = voices[i].name;
+      voiceSelect.appendChild(voice);
+    }
+  });
+
+  // Set the voice when one is chosen from the list
+  voiceSelect.addEventListener('change', () => {
+    let voiceOption = voiceSelect.options[voiceSelect.selectedIndex].value;
+    utterance.voice = getVoiceObject(voices, voiceOption);
+  });
+
+  // Start speaking when the PTT button is pressed and smile
+  pressToTalkBtn.addEventListener('click', () => {
+    utterance.text = document.getElementById("text-to-speak").value;
+    ss.speak(utterance);
+    checkIfSpeaking(ss, faceImg);   
+  });
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,28 +1,29 @@
 // expose.js
-
 window.addEventListener('DOMContentLoaded', init);
+const confetti = new JSConfetti(); // Confetti!
 
 function init() {
   let onpage_images = document.getElementsByTagName("img"); // Get the image element
-  let horn_img = onpage_images[0];
-  let horn_select = document.getElementById("horn-select"); // Get the select for choosing the horn
-  let volume_img = onpage_images[1];
-  let horn_volume_level = document.getElementById("volume"); // Get the volume level
 
+  let horn_select = document.getElementById("horn-select"); // Get the select for choosing the horn
+
+  let horn_volume_level = document.getElementById("volume"); // Get the volume level
+  let play_sound_btn = document.getElementsByTagName("button")[0]; // Get play button
+  let play_sound_audio = document.getElementsByClassName("hidden")[0]; // Get audio
+  
   // Add an event listener to change the horn
   horn_select.addEventListener('change', () => {
     let horn_option = horn_select.options[horn_select.selectedIndex].value; // Get the currently selected option
+    let horn_img = onpage_images[0];
+    
+    play_sound_audio.src = "assets/audio/" + horn_option + ".mp3";
     horn_img.src = "assets/images/" + horn_option + ".svg"; // Change the image being shown
-    if(horn_option == "party-horn"){
-      const confetti = new JSConfetti(); // Confetti should be bound to "Play Sound" button
-      confetti.addConfetti();
-    }
   });
 
-  // Change volume image depending on the 
-  // TODO: Still need to set <audio> volume
+  // Change volume image depending on the level
   horn_volume_level.addEventListener('change', () => {
     let currentVolume = horn_volume_level.value;
+    let volume_img = onpage_images[1];
     if(currentVolume == 0){
       volume_img.src = "assets/icons/volume-level-0.svg";
     }else if(currentVolume > 0 && currentVolume < 33){
@@ -31,6 +32,20 @@ function init() {
       volume_img.src = "assets/icons/volume-level-2.svg";
     }else{
       volume_img.src = "assets/icons/volume-level-3.svg";
+    }
+    play_sound_audio.volume = currentVolume / 100;
+  });
+
+  // "Play Sound" button listener
+  play_sound_btn.addEventListener('click', () => {
+    // Get the currently selected option
+    let horn_option = horn_select.options[horn_select.selectedIndex].value;
+    play_sound_audio.play();
+    if(horn_option == "party-horn"){
+      confetti.addConfetti({
+        confettiNumber: 1000,
+        confettiRadius: 10
+      });
     }
   });
 }
